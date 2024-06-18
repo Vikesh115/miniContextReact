84c87aad92282ac07ea9ef060bc233cdf502f826
Vikesh raut <Vikeshraut952@gmail.com>
2024-06-18 14:26:33 +0530

    useContext in React

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+// Fetch or recieve data using user from UserContext
+
+import React, {useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+function Profile() {
+    const {user} = useContext(UserContext)
+    
+    if (!user) return <div>please login</div>
+
+    return <div>Welcome {user.username}</div>
+}
+
+export default Profile
